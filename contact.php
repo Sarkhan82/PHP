78fb89f4cacec9f08d4dc1be710077afedb75a26
Sarkhan <david.jonquieres@gmail.com>
2022-09-06 16:48:15 +0200

    cours date

--- contact.php
+++ contact.php
@@ -2,8 +2,13 @@
 $title = "Nous contacter";
 require_once "header.php";
 require_once "menu_function.php";
+date_default_timezone_set("Europe/Paris");
+$heure = (int)($_GET["heure"] ?? date("G"));
+$jour = (int)($_GET["jour"] ?? date("N") - 1);
+$creneaux = CRENEAUX[$jour];
+$ouvert = in_creneaux($heure, $creneaux);
+$color = $ouvert ? "green" : "red";
 require_once "config.php";
-$creneaux = creneaux_html(CRENEAUX);
 $nav = "contact";
 require 'header.php';
 ?>
@@ -15,7 +20,32 @@ require 'header.php';
     </div>
     <div class="col-md-4">
         <h2>Horaire d'ouverture</h2>
-        <?= $creneaux ?>
+        <?php if ($ouvert) : ?>
+            <div class="alert alert-success">
+                Le magasin est ouvert
+            </div>
+        <?php else : ?>
+            <div class="alert alert-danger">
+                Le magasin est fermé
+            </div>
+        <?php endif; ?>
+        <form action="" method="get">
+            <div class="from-group">
+                <?= select("jour", $jour, JOURS) ?>
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="number" name="heure" value="<?= $heure ?>">
+            </div>
+            <button class="btn btn-primary" type="submit">Voir si le magasin est ouvert</button>
+        </form>
+        <ul>
+            <?php foreach (JOURS as $k => $jour) : ?>
+                <li <?php if ($k + 1 === (int)date("N")) : ?> style=<?= $color; ?> <?php endif; ?>>
+                    <strong> <?= $jour ?></strong>
+                    <?= creneaux_html(CRENEAUX[$k]); ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </div>
 
--- menu_function.php
+++ menu_function.php
@@ -33,6 +33,16 @@ function radio(string $name, string $value, array $data): string
 HTML;
 }
 
+function select(string $name, $value, array $options): string
+{
+  $html_options = [];
+  foreach ($options as $k => $option) {
+    $attributes = $k == $value ? "selected" : "";
+    $html_options[] = "<option value='$k' $attributes>$option</option>";
+  }
+  return "<select class='form-control' name='$name'>" . implode($html_options) . "</select>";
+}
+
 function dump($variable)
 {
   echo "<pre>";
@@ -43,8 +53,23 @@ function dump($variable)
 function creneaux_html(array $creneaux)
 {
   $phrases = [];
+  if (empty($creneaux)) {
+    return "Fermé";
+  }
   foreach ($creneaux as $creneau) {
     $phrases = "de <strong>{$creneau[0]}h</strong> à <strong>{$creneau[1]}h</strong>";
   }
   return "Ouvert de " . implode(" et ", $phrases);
+}
+
+function in_creneaux(int $heure, array $creneaux): bool
+{
+  foreach ($creneaux as $creneau) {
+    $debut = $creneau[0];
+    $fin = $creneau[1];
+    if ($heure >= $debut && $heure <= $fin) {
+      return true;
+    }
+  }
+  return false;
 }
